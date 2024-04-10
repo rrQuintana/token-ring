ea64fa94bc031adb83c657c21726f5f6371f65f2
Roberto Quintana <rrquintana7@gmail.com>
2024-04-10 15:03:58 -0600

    fix 2

--- Token-RIng.py
+++ Token-RIng.py
@@ -7,13 +7,11 @@ Roberto = '175.1.58.24'
 Oliver = '175.1.57.71'
 Jose = '175.1.61.58'
 
-
 # Dirección IP del host actual
 my_host = Roberto
 
 # Lista de direcciones IP de los otros hosts en el anillo
 hostList = [Oliver, Jose]
-
 
 # Puerto para la comunicación
 port = 12345
@@ -41,12 +39,19 @@ def send_token(host, port, token):
 
     client_socket.close()
 
+# Función para simular la ejecución de una tarea
+def execute_task():
+    print("Simulando ejecución de tarea...")
+    time.sleep(2)  # Simulamos una tarea que tarda 2 segundos en ejecutarse
+    print("Tarea completada.")
+
 # Función para manejar la simulación
 def start_simulation():
     print("Simulación iniciada.")
     while True:
+        execute_task()  # Simular la ejecución de una tarea
         for next_host in hostList:
-            send_token(next_host, port, "TOKEN")
+            send_token(next_host, port, "TOKEN")  # Envía el token al siguiente host
             time.sleep(1)  # Espera 1 segundo antes de enviar el token al siguiente host
 
 if __name__ == "__main__":
