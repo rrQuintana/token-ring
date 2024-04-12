947558fafecf5fd0693b897ffeefb504533f6c49
Roberto Quintana <rrquintana7@gmail.com>
2024-04-12 14:37:26 -0600

    server codigo duro

--- client.js
+++ client.js
@@ -6,30 +6,36 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Solicitar al usuario que ingrese la dirección IP del servidor
-rl.question('Ingresa la dirección IP del servidor: ', (ip) => {
-    // Conectar al servidor
-    const socket = io.connect(`http://${ip}`);
+// Dirección IP del servidor
+const serverIP = '127.0.0.1'; // Cambia esto con la IP de tu servidor
 
-    // Escuchar mensajes del servidor
-    socket.on('message', (data) => {
-        console.log('Mensaje del servidor:', data);
-    });
+// Conectar al servidor
+const socket = io.connect(`http://${serverIP}`);
 
-    // Leer entrada del usuario desde la consola y enviarla al servidor
-    rl.on('line', (input) => {
-        // Enviar mensaje al servidor
-        socket.emit('message', input);
-    });
+socket.on('connect', () => {
+    console.log('Conectado al servidor.');
+    // Enviar mensaje al servidor indicando que el cliente se ha conectado
+    socket.emit('message', 'Cliente conectado');
+});
 
-    // Manejar desconexión del servidor
-    socket.on('disconnect', () => {
-        console.log('Desconectado del servidor.');
-        rl.close();
-    });
+// Escuchar mensajes del servidor
+socket.on('message', (data) => {
+    console.log('Mensaje del servidor:', data);
+});
 
-    // Cerrar la interfaz de lectura cuando se desconecta el servidor
-    rl.on('close', () => {
-        process.exit(0);
-    });
+// Leer entrada del usuario desde la consola y enviarla al servidor
+rl.on('line', (input) => {
+    // Enviar mensaje al servidor
+    socket.emit('message', input);
+});
+
+// Manejar desconexión del servidor
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor.');
+    rl.close();
+});
+
+// Cerrar la interfaz de lectura cuando se desconecta el servidor
+rl.on('close', () => {
+    process.exit(0);
 });
