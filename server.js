0f6601b0621ed970963ee258c8b88bea58ec2a1b
Oliver Aton <oliver.gil.estrada9@gmail.com>
2024-04-12 15:49:34 -0600

    server enter

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const socketio = require('socket.io');
+const readline = require('readline');
 
+// Crear servidor HTTP básico
 const server = http.createServer((req, res) => {
     res.end('Token Ring Server');
 });
@@ -8,6 +10,12 @@ const server = http.createServer((req, res) => {
 const io = socketio(server);
 const clients = [];
 const clientStrengths = {};
+
+// Interfaz de línea de comandos para iniciar el envío del token
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 io.on('connection', (socket) => {
     console.log(`Cliente conectado: ${socket.id}`);
@@ -57,4 +65,12 @@ function performLeaderElection() {
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log('Presione Enter para comenzar a enviar el token...');
+    rl.on('line', (input) => {
+        if (clients.length > 0) {
+            clients[0].emit('token', 'Inicio del token');
+        } else {
+            console.log('No hay clientes conectados para iniciar el token.');
+        }
+    });
 });
