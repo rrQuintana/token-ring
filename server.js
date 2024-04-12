929f487300f380dc671bf362fb465577e826e3c4
Oliver Aton <oliver.gil.estrada9@gmail.com>
2024-04-12 16:08:13 -0600

    ti,e

--- server.js
+++ server.js
@@ -48,7 +48,16 @@ function passToken(socket) {
     const nextIndex = (index + 1) % clients.length;
     if (clients[nextIndex]) {
         console.log(`Pasando token al cliente ${clients[nextIndex].id}`);
-        clients[nextIndex].emit('token', 'Tienes el token');
+        clients[nextIndex].emit('message', 'Realizando tarea...');
+        setTimeout(() => {
+            clients[nextIndex].emit('message', 'Haciendo tarea...');
+            setTimeout(() => {
+                clients[nextIndex].emit('message', 'Finalizando tarea...');
+                setTimeout(() => {
+                    clients[nextIndex].emit('token', 'Tienes el token');
+                }, 1000);
+            }, 1000);
+        }, 1000);
     }
 }
 
@@ -68,7 +77,10 @@ server.listen(PORT, () => {
     console.log('Presione Enter para comenzar a enviar el token...');
     rl.on('line', (input) => {
         if (clients.length > 0) {
-            clients[0].emit('token', 'Inicio del token');
+            clients[0].emit('message', 'Inicio del token');
+            setTimeout(() => {
+                clients[0].emit('token', 'Tienes el token');
+            }, 4000); // Dar tiempo para mostrar todos los mensajes de "Inicio del token"
         } else {
             console.log('No hay clientes conectados para iniciar el token.');
         }
